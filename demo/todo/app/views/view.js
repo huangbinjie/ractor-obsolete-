0862dadfbf8ec6640893b8fd487d3c82c85dc388
bingjie.huang <bingjie.huang@shuyun.com>
2017-06-20 17:04:30 +0800

    remove js file

--- demo/todo/app/views/view.js
+++
@@ -1,7 +0,0 @@
-class Foo {
-}
-const div = Ractor.createElement("div", null,
-    Ractor.createElement("span", null, "111"),
-    Ractor.createElement("span", null, "222"));
-const foo = Ractor.createElement(Foo, { href: "1" },
-    Ractor.createElement("span", null, "11"));
--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var renderer_1 = require("./renderer");
-exports.render = renderer_1.render;
-var component_1 = require("./component");
-exports.Component = component_1.Component;
-var createElement_1 = require("./createElement");
-exports.createElement = createElement_1.createElement;
